10648c453a024a3ccc4a9f7cfca588119731f22d
JairoBoss <61020858+JairoBoss@users.noreply.github.com>
2020-04-30 10:12:53 -0500

    Corregido :)

--- archivos.c
+++ archivos.c
@@ -1,21 +1,63 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-int main()
+char *trim(char *str)
 {
-	FILE *aarchivo;
-	char linea[100];
-	aarchivo = fopen("ids.txt", "r");
-	if(aarchivo == NULL)
-	{
-		printf("Error al abrir el archivo");
-		return(1);
-	}
-	while( fgets ( linea, 10, aarchivo ) != NULL )
+    size_t len = 0;
+    char *frontp = str;
+    char *endp = NULL;
+
+    if( str == NULL ) { return NULL; }
+    if( str[0] == '\0' ) { return str; }
+
+    len = strlen(str);
+    endp = str + len;
+
+
+    while( isspace((unsigned char) *frontp) ) { ++frontp; }
+    if( endp != frontp )
     {
-        fscanf(aarchivo,"%s",linea);
-        printf("0%s @ulsaoaxaca.edu.mx\n", linea);
+        while( isspace((unsigned char) *(--endp)) && endp != frontp ) {}
     }
 
-	fclose(aarchivo);
+    if( frontp != str && endp == frontp )
+            *str = '\0';
+    else if( str + len - 1 != endp )
+            *(endp + 1) = '\0';
+
+
+    endp = str;
+    if( frontp != str )
+    {
+            while( *frontp ) { *endp++ = *frontp++; }
+            *endp = '\0';
+    }
+
+    return str;
+}
+
+int main(int argc, char const *argv[])
+{
+    FILE *aarchivo;
+    char linea[10];
+
+    aarchivo = fopen("ids.txt", "r");
+    if(aarchivo == NULL)
+    {
+        printf("Error al abrir archivo.\n");
+        return(1);
+    }
+
+    printf("Estas son las direcciones de correo de los alumnos de la clase:\n");
+    while( fgets( linea, 10, aarchivo ) != NULL )
+    {
+
+        trim(linea);
+
+        if(strlen(linea) > 0)
+            printf("0%s@ulsaoaxaca.edu.mx\n" , linea) ;
+    }
+    fclose(aarchivo);
 }
